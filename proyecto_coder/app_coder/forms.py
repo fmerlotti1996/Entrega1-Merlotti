73ef62f7dbe77e093c41f4c799a3ff6ab2118723
Francisco Merlotti <104647653+fmerlotti1996@users.noreply.github.com>
2022-06-02 21:25:32 -0300

    Add files via upload

---
+++ proyecto_coder/app_coder/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+
+
+class Curso_formulario(forms.Form):
+
+    nombre = forms.CharField(max_length=40)
+    camada = forms.IntegerField()
+
+class Alumno_formulario(forms.Form):
+
+    nombre = forms.CharField(max_length=40)
+    camada = forms.IntegerField()
+    nacimiento = forms.DateField()
+
+class Profesores_formulario(forms.Form):
+
+    nombre = forms.CharField(max_length=40)
+    Legajo = forms.IntegerField()
